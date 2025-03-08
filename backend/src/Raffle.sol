29b1de96010ae3f071417dc0c739e8dcee4a824e
devanganaB <devanganabarua@gmail.com>
2025-03-08 16:34:57 +0530

    landing page raffle UI

--- backend/src/Raffle.sol
+++ backend/src/Raffle.sol
@@ -21,7 +21,7 @@
 
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
@@ -154,7 +154,7 @@ contract Raffle is VRFConsumerBaseV2Plus, AutomationCompatibleInterface {
                 )
             })
         );
-        // Quiz... is this redundant?
+       
         emit RequestedRaffleWinner(requestId);
     }
 
