6178b2846259d7a776edd0c14dee8ffe2f701299
Learnedprawn <vineshparalkar@gmail.com>
2025-03-09 09:24:49 +0530

    getBlockTimestamp

--- backend/src/Raffle.sol
+++ backend/src/Raffle.sol
@@ -245,4 +245,10 @@ contract Raffle is VRFConsumerBaseV2Plus, AutomationCompatibleInterface {
     function getPlayerEntries(address player) public view returns (uint256) {
         return s_playerEntries[player];
     }
+
+    function getBlockTimeStamp() public view returns (uint256) {
+        return block.timestamp;
+    }
+
+    
 }
